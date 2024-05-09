aa05d0f3dd0da3cd4020f1ff89b9671f89f607f9
ivannalisetska <155262606+ivannalisetska@users.noreply.github.com>
2024-05-09 14:22:51 -0600

    Update script.sh

--- tests/script.sh
+++ tests/script.sh
@@ -3,7 +3,7 @@
 # Check if the retry count environment variable is set and greater than 0
 if [[ "$BUILDKITE_RETRY_COUNT" -gt 0 ]]; then
     echo "This is a retry, sleeping for 30 seconds."
-    sleep 30
+    exit -1
 else
     echo "This is the first attempt, sleeping for 120 seconds."
     sleep 120
