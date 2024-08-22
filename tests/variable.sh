f6bf46ef6e7690d3c9643b7b940d9a36a0f31269
ivannalisetska <155262606+ivannalisetska@users.noreply.github.com>
2024-08-22 15:45:55 -0600

    Create variable.sh

---
+++ tests/variable.sh
@@ -0,0 +1,23 @@
+# Determine the value of MY_BOOLEAN_VAR
+if [ "$MY_BOOLEAN_VAR" = "true" ]; then
+  cat <<EOF
+steps:
+  - label: "Dynamic Sleep Test"
+    command: |
+      sleep 5
+    retry:
+      manual: true
+EOF
+else
+  cat <<EOF
+steps:
+  - label: "Dynamic Sleep Test (No Retry)"
+    command: |
+      sleep 5
+    retry:
+      manual: false
+EOF
+fi
+
+env:
+  MY_BOOLEAN_VAR: "true"
